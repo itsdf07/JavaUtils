076a72c5a98cac29f864a5f5e874210b43959fb4
itsdf07 <923255742@qq.com>
2019-08-22 23:17:32 +0800

    优化算法中的输出方式，以及添加基础的几种算法截图

--- src/com/utils/SortUtils.java
+++ src/com/utils/SortUtils.java
@@ -1,5 +1,7 @@
 package com.utils;
 
+import java.util.Arrays;
+
 public class SortUtils {
 
     public static void main(String[] args) {
@@ -12,15 +14,15 @@ public class SortUtils {
     public static void onBubbleSort() {
         int temp;
         int[] datas = {10, 1, 18, 30, 23, 12, 7, 5, 18, 17};
-        System.out.print("原始数据：");
-        for (int data :
-                datas) {
-            System.out.print(data + " ");
-        }
+        System.out.println("原始数据：" + Arrays.toString(datas));
+//        for (int data :
+//                datas) {
+//            System.out.print(data + " ");
+//        }
         for (int i = 0; i < datas.length; i++) {
             //优化部分就是j < datas.length - (i + 1)，因为挡轮最大/最小已经被移至最后，所以不用在管他们了
             for (int j = 0; j < datas.length - (i + 1); j++) {
-                if (datas[j] < datas[j + 1]) {
+                if (datas[j] > datas[j + 1]) {
                     temp = datas[j];
                     datas[j] = datas[j + 1];
                     datas[j + 1] = temp;
@@ -28,10 +30,6 @@ public class SortUtils {
             }
         }
         System.out.println();
-        System.out.print("冒泡排序后的数据：");
-        for (int data :
-                datas) {
-            System.out.print(data + " ");
-        }
+        System.out.print("冒泡排序后的数据：" + Arrays.toString(datas));
     }
 }
